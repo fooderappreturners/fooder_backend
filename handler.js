b8b2306e9041c270131b87d9efd925d6397d154d
Alberto MDV <albertomdv@gmail.com>
2019-12-13 12:54:39 +0000

    configuration of backend project and test endpoint

--- handler.js
+++ handler.js
@@ -1,18 +1,34 @@
-'use strict';
+const express = require("express");
+const serverless = require("serverless-http");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const app = express();
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+app.use(cors());
+app.use(bodyParser.json());
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "fooder"
+});
+
+app.get("/fooder", function(req, res) {
+  connection.query("SELECT * FROM restaurants", function(err, data) {
+    if (err) {
+      console.log("Error fetching tasks", err);
+      res.status(500).json({
+        error: err
+      });
+    } else {
+      res.json({
+        restaurants: data
+      });
+    }
+  });
+});
+
+module.exports.fooder = serverless(app);
