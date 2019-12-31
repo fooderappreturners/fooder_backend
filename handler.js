04fe267b1fbb24e351ab58cf74f3cd8555e8e654
Lydia Joynes <lydiajoynesgmail.com>
2019-12-31 13:47:59 +0000

    implemented dietaryOptions get request

--- handler.js
+++ handler.js
@@ -16,8 +16,24 @@ const connection = mysql.createConnection({
   database: "fooder"
 });
 
+app.get("dietaryOptions", function (req, res) {
+  const sql = `SELECT * FROM dietaryOptions`;
+  connection.query(sql, (err, data) => {
+    if (err) {
+      console.log("Error fetching dietary options", err);
+      res.status(500).json({
+        error: err
+      });
+    } else {
+      res.json({
+        dietaryOptions: data
+      });
+    }
+  });
+});
+
 //Return the restaurants whith a specific dietaryOption
-app.get("/restaurants/:dietaryOptionId", function(req, res) {
+app.get("/restaurants/:dietaryOptionId", function (req, res) {
 
   const dietaryOptionId = req.params.dietaryOptionId;
 
@@ -41,20 +57,20 @@ app.get("/restaurants/:dietaryOptionId", function(req, res) {
 });
 
 //Return the restaurants whith a multiply selection dietaryOptions
-app.get("/multipleRestaurant/:dietaryOptionId1/:dietaryOptionId2/:dietaryOptionId3/:dietaryOptionId4/:dietaryOptionId5", function(req, res) {
-  
-/***In Postman not working if it missing a parameter, check how to work in the front ***/
+app.get("/multipleRestaurant/:dietaryOptionId1/:dietaryOptionId2/:dietaryOptionId3/:dietaryOptionId4/:dietaryOptionId5", function (req, res) {
+
+  /***In Postman not working if it missing a parameter, check how to work in the front ***/
   const dietaryOptionId1 = req.params.dietaryOptionId1;
   const dietaryOptionId2 = req.params.dietaryOptionId2;
   const dietaryOptionId3 = req.params.dietaryOptionId3;
   const dietaryOptionId4 = req.params.dietaryOptionId4;
   const dietaryOptionId5 = req.params.dietaryOptionId5;
 
-  if (typeof dietaryOptionId1 === 'undefined') dietaryOptionId1= null;
-  if (typeof dietaryOptionId2 === 'undefined') dietaryOptionId2= null;
-  if (typeof dietaryOptionId3 === 'undefined') dietaryOptionId3= null;
-  if (typeof dietaryOptionId4 === 'undefined') dietaryOptionId4= null;
-  if (typeof dietaryOptionId5 === 'undefined') dietaryOptionId5= null;
+  if (typeof dietaryOptionId1 === 'undefined') dietaryOptionId1 = null;
+  if (typeof dietaryOptionId2 === 'undefined') dietaryOptionId2 = null;
+  if (typeof dietaryOptionId3 === 'undefined') dietaryOptionId3 = null;
+  if (typeof dietaryOptionId4 === 'undefined') dietaryOptionId4 = null;
+  if (typeof dietaryOptionId5 === 'undefined') dietaryOptionId5 = null;
 
   const sql = `SELECT DISTINCT r.id, r.name, r.capacity, r.location, r.description, r.phoneNumber, r.img 
                 FROM restaurants r 
@@ -66,7 +82,7 @@ app.get("/multipleRestaurant/:dietaryOptionId1/:dietaryOptionId2/:dietaryOptionI
                 OR rdo.dietaryOption_id = ?
                 OR rdo.dietaryOption_id = ?`;
 
-  connection.query(sql, [dietaryOptionId1,dietaryOptionId2,dietaryOptionId3, dietaryOptionId4, dietaryOptionId5], (err, data) => {
+  connection.query(sql, [dietaryOptionId1, dietaryOptionId2, dietaryOptionId3, dietaryOptionId4, dietaryOptionId5], (err, data) => {
     if (err) {
       console.log("Error fetching restaurants", err);
       res.status(500).json({
@@ -81,7 +97,7 @@ app.get("/multipleRestaurant/:dietaryOptionId1/:dietaryOptionId2/:dietaryOptionI
 });
 
 //Return all bookings
-app.get("/bookings", function(req, res) {  
+app.get("/bookings", function (req, res) {
 
   const sql = `SELECT * FROM bookings`;
 
@@ -100,7 +116,7 @@ app.get("/bookings", function(req, res) {
 });
 
 //Return the bookings filtered by restaurant id
-app.get("/bookings/:restaurantId", function(req, res) {
+app.get("/bookings/:restaurantId", function (req, res) {
 
   const restaurantId = req.params.restaurantId;
 
@@ -121,7 +137,7 @@ app.get("/bookings/:restaurantId", function(req, res) {
 });
 
 //Add a booking
-app.post("/addBooking", function(req, res) {
+app.post("/addBooking", function (req, res) {
 
   const id = null;
   const date = req.body.date;
@@ -138,12 +154,12 @@ app.post("/addBooking", function(req, res) {
         error: err
       });
     } else {
-      res.status(201).json({   
-        id: res.insertId,         
+      res.status(201).json({
+        id: res.insertId,
         date,
         number,
         name,
-        restaurantId    
+        restaurantId
       });
     }
   });
@@ -164,12 +180,12 @@ app.delete("/deleteBooking/:bookingId", function (request, response) {
       });
     } else {
       response.status(200).json('Removal done');
-    }  
+    }
   });
 });
 
 //Return the bookings filtered by restaurant id and date
-app.get("/bookingsRestaurantsDate/:restaurantId/:date", function(req, res) {
+app.get("/bookingsRestaurantsDate/:restaurantId/:date", function (req, res) {
 
   const restaurantId = req.params.restaurantId;
   const date = req.params.date;
