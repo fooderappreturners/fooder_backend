a16077e6d01783fbda13c03fda3e0fa23faa3df5
Lydia Joynes <lydiajoynesgmail.com>
2019-12-31 22:13:36 +0000

    updates to serverless api

--- handler.js
+++ handler.js
@@ -31,29 +31,33 @@ app.get("/dietaryOptions", function (req, res) {
   });
 });
 
-// //Return the restaurants whith a specific dietaryOption
-// app.get("/restaurants/:dietaryOptionId", function (req, res) {
+//Return the restaurants whith a specific dietaryOption
+app.get("/restaurants/:dietaryOptionId", function (req, res) {
 
-//   const dietaryOptionId = req.params.dietaryOptionId;
+  const dietaryOptionId = req.params.dietaryOptionId.split(',');
+  let placeholders = [];
+  dietaryOptionId.forEach(id => {
+    placeholders.push('?');
+  });
 
-//   const sql = `SELECT r.id, r.name, r.capacity, r.location, r.description, r.phoneNumber, r.img FROM restaurants r 
-//                 INNER JOIN restaurantsDietaryOptions rdo 
-//                 ON r.id=rdo.restaunrant_id 
-//                 WHERE rdo.dietaryOption_id = ?`;
+  const sql = `SELECT r.id, r.name, r.capacity, r.location, r.description, r.phoneNumber, r.img FROM restaurants r 
+                INNER JOIN restaurantsDietaryOptions rdo 
+                ON r.id=rdo.restaurant_id 
+                WHERE rdo.dietaryOption_id IN (${placeholders.join(',')})`;
 
-//   connection.query(sql, [dietaryOptionId], (err, data) => {
-//     if (err) {
-//       console.log("Error fetching restaurants", err);
-//       res.status(500).json({
-//         error: err
-//       });
-//     } else {
-//       res.json({
-//         restaurants: data
-//       });
-//     }
-//   });
-// });
+  connection.query(sql, dietaryOptionId, (err, data) => {
+    if (err) {
+      console.log("Error fetching restaurants", err);
+      res.status(500).json({
+        error: err
+      });
+    } else {
+      res.json({
+        restaurants: data
+      });
+    }
+  });
+});
 
 // //Return the restaurants whith a multiply selection dietaryOptions
 // app.get("/multipleRestaurant/:dietaryOptionId1/:dietaryOptionId2/:dietaryOptionId3/:dietaryOptionId4/:dietaryOptionId5", function (req, res) {
@@ -74,7 +78,7 @@ app.get("/dietaryOptions", function (req, res) {
 //   const sql = `SELECT DISTINCT r.id, r.name, r.capacity, r.location, r.description, r.phoneNumber, r.img 
 //                 FROM restaurants r 
 //                 INNER JOIN restaurantsDietaryOptions rdo 
-//                 ON r.id=rdo.restaunrant_id 
+//                 ON r.id=rdo.restaurant_id 
 //                 WHERE rdo.dietaryOption_id = ?
 //                 OR rdo.dietaryOption_id = ?
 //                 OR rdo.dietaryOption_id = ?
@@ -95,114 +99,114 @@ app.get("/dietaryOptions", function (req, res) {
 //   });
 // });
 
-// //Return all bookings
-// app.get("/bookings", function (req, res) {
+//Return all bookings
+app.get("/bookings", function (req, res) {
 
-//   const sql = `SELECT * FROM bookings`;
+  const sql = `SELECT * FROM bookings`;
 
-//   connection.query(sql, (err, data) => {
-//     if (err) {
-//       console.log("Error fetching bookings", err);
-//       res.status(500).json({
-//         error: err
-//       });
-//     } else {
-//       res.json({
-//         bookings: data
-//       });
-//     }
-//   });
-// });
+  connection.query(sql, (err, data) => {
+    if (err) {
+      console.log("Error fetching bookings", err);
+      res.status(500).json({
+        error: err
+      });
+    } else {
+      res.json({
+        bookings: data
+      });
+    }
+  });
+});
 
-// //Return the bookings filtered by restaurant id
-// app.get("/bookings/:restaurantId", function (req, res) {
+//Return the bookings filtered by restaurant id
+app.get("/bookings/:restaurantId", function (req, res) {
 
-//   const restaurantId = req.params.restaurantId;
+  const restaurantId = req.params.restaurantId;
 
-//   const sql = `SELECT * FROM bookings WHERE restaunrant_id= ?`;
+  const sql = `SELECT * FROM bookings WHERE restaurant_id= ?`;
 
-//   connection.query(sql, [restaurantId], (err, data) => {
-//     if (err) {
-//       console.log("Error fetching restaurants", err);
-//       res.status(500).json({
-//         error: err
-//       });
-//     } else {
-//       res.json({
-//         restaurants: data
-//       });
-//     }
-//   });
-// });
+  connection.query(sql, [restaurantId], (err, data) => {
+    if (err) {
+      console.log("Error fetching restaurants", err);
+      res.status(500).json({
+        error: err
+      });
+    } else {
+      res.json({
+        restaurants: data
+      });
+    }
+  });
+});
 
-// //Add a booking
-// app.post("/addBooking", function (req, res) {
+//Add a booking
+app.post("/addBooking", function (req, res) {
 
-//   const id = null;
-//   const date = req.body.date;
-//   const number = req.body.number;
-//   const name = req.body.name;
-//   const restaurantId = req.body.restaurantId;
+  const id = null;
+  const date = req.body.date;
+  const number = req.body.number;
+  const name = req.body.name;
+  const restaurantId = req.body.restaurantId;
 
-//   const sql = `INSERT INTO bookings VALUES (?, ?, ?, ?, ?)`;
+  const sql = `INSERT INTO bookings VALUES (?, ?, ?, ?, ?)`;
 
-//   connection.query(sql, [id, date, number, name, restaurantId], (err) => {
-//     if (err) {
-//       console.log("Error adding a booking", err);
-//       res.status(500).json({
-//         error: err
-//       });
-//     } else {
-//       res.status(201).json({
-//         id: res.insertId,
-//         date,
-//         number,
-//         name,
-//         restaurantId
-//       });
-//     }
-//   });
-// });
+  connection.query(sql, [id, date, number, name, restaurantId], (err) => {
+    if (err) {
+      console.log("Error adding a booking", err);
+      res.status(500).json({
+        error: err
+      });
+    } else {
+      res.status(201).json({
+        id: res.insertId,
+        date,
+        number,
+        name,
+        restaurantId
+      });
+    }
+  });
+});
 
-// //Delete a booking
-// app.delete("/deleteBooking/:bookingId", function (request, response) {
-//   const id = request.params.bookingId;
+//Delete a booking
+app.delete("/deleteBooking/:bookingId", function (request, response) {
+  const id = request.params.bookingId;
 
-//   const sql = 'DELETE FROM bookings WHERE id = ?';
+  const sql = 'DELETE FROM bookings WHERE id = ?';
 
-//   connection.query(sql, [id], (err, results, fields) => {
+  connection.query(sql, [id], (err, results, fields) => {
 
-//     if (err) {
-//       console.log("Error deleting a booking", err);
-//       response.status(500).json({
-//         error: err
-//       });
-//     } else {
-//       response.status(200).json('Removal done');
-//     }
-//   });
-// });
+    if (err) {
+      console.log("Error deleting a booking", err);
+      response.status(500).json({
+        error: err
+      });
+    } else {
+      response.status(200).json('Removal done');
+    }
+  });
+});
 
-// //Return the bookings filtered by restaurant id and date
-// app.get("/bookingsRestaurantsDate/:restaurantId/:date", function (req, res) {
+//Return the bookings filtered by restaurant id and date
+app.get("/bookingsRestaurantsDate/:restaurantId/:date", function (req, res) {
 
-//   const restaurantId = req.params.restaurantId;
-//   const date = req.params.date;
+  const restaurantId = req.params.restaurantId;
+  const date = req.params.date;
 
-//   const sql = `SELECT * FROM bookings WHERE restaunrant_id = ? AND date = ?`;
+  const sql = `SELECT * FROM bookings WHERE restaurant_id = ? AND date = ?`;
 
-//   connection.query(sql, [restaurantId, date], (err, data) => {
-//     if (err) {
-//       console.log("Error fetching bookings", err);
-//       res.status(500).json({
-//         error: err
-//       });
-//     } else {
-//       res.json({
-//         bookings: data
-//       });
-//     }
-//   });
-// });
+  connection.query(sql, [restaurantId, date], (err, data) => {
+    if (err) {
+      console.log("Error fetching bookings", err);
+      res.status(500).json({
+        error: err
+      });
+    } else {
+      res.json({
+        bookings: data
+      });
+    }
+  });
+});
 
 module.exports.fooder = serverless(app);
